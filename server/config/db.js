21c2be424ad984fda1736d26af3550e87249ce4c
pranshu <singhpranshu950@gmail.com>
2025-03-20 06:22:19 +0530

    changed

--- server/config/db.js
+++ server/config/db.js
@@ -5,6 +5,8 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, 
+    socketTimeoutMS: 45000,
     });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
